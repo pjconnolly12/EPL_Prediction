f04e6b0f60355c45b5c32f39cc154dbe92135a70
unknown <pjconnolly12@gmail.com>
2020-02-24 20:43:33 -0500

    updated build

--- index.js
+++ index.js
@@ -9,11 +9,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, “../client/build”)));
-
-app.get(“*”, (req, res) => {
-res.sendFile(path.join(__dirname + “../client/build/index.html”));
-});
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
@@ -38,6 +34,10 @@ app.use('/teams', teamsRouter);
 app.use('/forgotpassword', forgotRouter);
 app.use('/reset', resetRouter);
 app.use('/updatepassword', updatepasswordRouter)
+
+app.get('*', (req, res) => {
+res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 app.listen(port, () => {
 	console.log(`Server is running on port: ${port}`);
